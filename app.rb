db85561e45028ba79f1df671571a0c3b24030c79
Tammy Metz <tammy@tammymetz.com>
2018-11-03 23:25:40 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Tammy"
+  end
+
+  get '/hometown' do
+    "My hometown is Annandale"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is The Pretender"
+  end
 end
